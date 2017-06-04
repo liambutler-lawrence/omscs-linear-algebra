365d795e47e77077ee6f6ba893fec3bbf9f548b1
Liam Butler-Lawrence <liamdunn@icloud.com>
2017-06-04 15:18:10 -0400

    Implement plus, minus, and scalar multiply functions for vectors

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -19,3 +21,32 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+
+    def __add__(self, v):
+        if self.dimension != v.dimension:
+            raise ValueError('The vectors to add must be in the same dimension')
+
+        sum = []
+        for i in range(0, self.dimension):
+            sum.append(self.coordinates[i] + v.coordinates[i])
+
+        return Vector(sum)
+
+
+    def __sub__(self, v):
+    	if self.dimension != v.dimension:
+    		raise ValueError('The vectors to subtract must be in the same dimension')
+
+    	difference = []
+    	for i in range(0, self.dimension):
+    		difference.append(self.coordinates[i] - v.coordinates[i])
+
+    	return Vector(difference)
+
+    def __mul__(self, scalar):
+    	product = []
+    	for i in range(0, self.dimension):
+    		product.append(self.coordinates[i] * scalar)
+
+    	return Vector(product)
