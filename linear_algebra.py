c57d4c1cdb4b191b6dd6e1ffdb7720e2877ec900
Liam Butler-Lawrence <liamdunn@icloud.com>
2017-06-05 19:18:01 -0400

    Add new helper class to define quiz input

---
+++ linear_algebra.py
@@ -0,0 +1,19 @@
+from vector import Vector
+
+def print_quiz(name):
+    print
+    print '*****'
+    print name
+    print
+
+
+print_quiz('Quiz: Plus, Minus, Scalar Multiply')
+
+print 'Q1'
+print Vector([8.218, -9.341]) + Vector([-1.129, 2.111])
+
+print 'Q2'
+print Vector([7.119, 8.215]) - Vector([-8.223, 0.878])
+
+print 'Q3'
+print Vector([1.671, -1.012, -0.318]) * 7.41
