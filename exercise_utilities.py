a4b58c44a518c85ee64f324a76fd691984f38aaf
Liam Butler-Lawrence <liamdunn@icloud.com>
2017-06-11 16:27:59 -0400

    Restructure exercise-executing code into one file per lesson + a utilities file

---
+++ exercise_utilities.py
@@ -0,0 +1,16 @@
+def print_info(caption):
+    print
+    print
+    print caption + ' LINEAR ALGEBRA QUIZ LOG'
+    print
+
+
+def print_quiz(name):
+    print
+    print '*****'
+    print 'Quiz: ' + name
+
+
+def print_question(name):
+    print
+    print name + ':'
--- exercises_vectors.py
+++ exercises_vectors.py
@@ -1,20 +1,5 @@
+from exercise_utilities import *
 from vector import Vector
-
-def print_info(caption):
-    print
-    print
-    print caption + ' LINEAR ALGEBRA QUIZ LOG'
-    print
-
-def print_quiz(name):
-    print
-    print '*****'
-    print 'Quiz: ' + name
-
-
-def print_question(name):
-    print
-    print name + ':'
 
 
 print_info('BEGIN')
