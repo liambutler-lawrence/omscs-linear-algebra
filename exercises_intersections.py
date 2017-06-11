002f3074d0886177cc565725d26a682310757685
Liam Butler-Lawrence <liamdunn@icloud.com>
2017-06-11 16:37:54 -0400

    Define exercise structure for 2D intersections quiz

---
+++ exercises_intersections.py
@@ -0,0 +1,17 @@
+from exercise_utilities import *
+from line import Line
+
+
+print_info('BEGIN')
+
+
+print_quiz('Coding Functions for Lines')
+
+print_question('Intersection 1')
+
+print_question('Intersection 2')
+
+print_question('Intersection 3')
+
+
+print_info('END')
