ea098a43cacea18596324ca17a3da2e285fb7df2
Liam Butler-Lawrence <liamdunn@icloud.com>
2017-07-01 15:52:53 -0400

    Implement initial version of Parametrization from Udacity course video

---
+++ parametrization.py
@@ -0,0 +1,26 @@
+from decimal import Decimal, getcontext
+
+from vector import Vector
+
+
+getcontext().prec = 30
+
+
+class Parametrization(object):
+
+
+    BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG = 'The basepoint and direction vectors should all live in the same dimension'
+
+
+    def __init__(self, basepoint, direction_vectors):
+
+        self.basepoint = basepoint
+        self.direction_vectors = direction_vectors
+        self.dimension = self.basepoint.dimension
+
+        try:
+            for v in direction_vectors:
+                assert v.dimension == self.dimension
+
+        except AssertionError:
+            raise Exception(self.BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
